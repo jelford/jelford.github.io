7d4e53ad716484e83ebb2443fa403ded48a2be10
James Elford <james.p.elford@gmail.com>
2014-01-05 11:03:45 +0000

    Trying including the script directly

---
+++ scripts/onion-image-load.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// // // @name        Load Onion Images
+// // // @namespace   com.jameselford.scripts
+// // // @description Surrogate script to properly load images from The Onion
+// // // @include     http://www.theonion.com/*
+// // // @version     1
+// // // @grant       none
+// // // @downloadURL 
+// // // ==/UserScript==
+
+alert('hi')
